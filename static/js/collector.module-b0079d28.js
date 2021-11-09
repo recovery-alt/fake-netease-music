fc61e7905c5b492b1a58319ae726d93e1b2dacbb
cq360767996 <44747765+cq360767996@users.noreply.github.com>
2021-11-09 13:04:48 +0000

    Deploying to gh-pages from  @ 456ddd268a211b1f78f23fff20c1e12d1297873a 🚀

---
+++ static/js/collector.module-b0079d28.js
@@ -0,0 +1,1 @@
+const o="_collector_1w6oh_1",_="_collector__item_1w6oh_6",t="_collector__img_1w6oh_12",l="_collector__title_1w6oh_18",c="_collector__subtitle_1w6oh_25",e="_collector__empty_1w6oh_30",r="_collector__footer_1w6oh_35";var s={collector:o,collector__item:_,collector__img:t,collector__title:l,collector__subtitle:c,collector__empty:e,collector__footer:r};export{s};
