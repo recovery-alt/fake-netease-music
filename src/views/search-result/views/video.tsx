bd61afc433f1b5c27af64f8692bf4f0bd3a02009
chengcq <chengcq@getui.com>
2022-03-23 19:48:42 +0800

    feat: 歌手页搜索结果页mv跳转

--- src/views/search-result/views/video.tsx
+++ src/views/search-result/views/video.tsx
@@ -6,17 +6,24 @@ import styles from './video.module.less';
 import Img from '@/components/img';
 import { classGenerator, resizeImg } from '@/utils';
 import classNames from 'classnames';
+import { useHistory } from 'react-router-dom';
+import { DynamicPage } from '@/router';
 
 const getClass = classGenerator('video', styles);
 
 const Video: React.FC<Props> = props => {
   const params = { ...props, currentType: SearchType.VIDEO, limit: 21 };
   const { wrapEmpty } = usePagination<SearchVideo>(params);
+  const { push } = useHistory();
+
+  function toPlayVideoPage(id: number | string) {
+    push(DynamicPage.playVideo(id));
+  }
 
   return wrapEmpty(data => (
     <div className={getClass()}>
       {data.videos.map(item => (
-        <div key={item.vid} className={getClass('item')}>
+        <div key={item.vid} className={getClass('item')} onClick={() => toPlayVideoPage(item.vid)}>
           <Img src={resizeImg(item.coverUrl, 460, 260)} className={getClass('img')} />
           <div className={getClass('description')}>
             <h2 className={classNames({ [styles['--mv']]: item.type === 0 })}>{item.title}</h2>
--- src/views/singer/mv/index.tsx
+++ src/views/singer/mv/index.tsx
@@ -4,25 +4,33 @@ import Img from '@/components/img';
 import { getArtistMV } from '@/api';
 import { MV as MVType } from '@/types';
 import { classGenerator, resizeImg } from '@/utils';
+import { DynamicPage } from '@/router';
+import { useHistory } from 'react-router-dom';
 
 type Props = { id: number };
 
 const MV: React.FC<Props> = ({ id }) => {
   const getClass = classGenerator('mv', styles);
   const [mvs, setMVs] = useState<MVType[]>([]);
+  const { push } = useHistory();
 
   async function loadArtistMV() {
     const res = await getArtistMV(id);
     setMVs(res.mvs);
   }
 
+  function toPlayVideoPage(id: number | string) {
+    push(DynamicPage.playVideo(id));
+  }
+
   useEffect(() => {
     loadArtistMV();
   }, [id]);
+
   return (
     <div className={getClass()}>
       {mvs.map(item => (
-        <div key={item.id} className={getClass('item')}>
+        <div key={item.id} className={getClass('item')} onClick={() => toPlayVideoPage(item.id)}>
           <Img src={resizeImg(item.imgurl, 230, 130)} className={getClass('img')} />
           <div className={getClass('description')}>{item.name}</div>
         </div>
